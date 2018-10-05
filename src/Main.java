fa09d0ff8ea417d4d8ad78afd4db6577d0855e4b
tikiwings <donaldsanchez7870@gmail.com>
2018-10-05 10:58:17 -0700

    travis

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(multTwo(4, 8));
+	}
+	public static int multTwo(int a, int b)
+	{
+		return a * b;
+	}
+}
